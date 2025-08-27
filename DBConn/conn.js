58311ff75ca0acaa054898196e3c1a54c7c76f0a
raghavguptaa7 <guptaraghav2277@gmail.com>
2025-08-27 18:47:02 +0530

    Update conn.js

--- DBConn/conn.js
+++ DBConn/conn.js
@@ -1,6 +1,16 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+require('dotenv').config(); // Ensures .env variables are loaded
 
-mongoose.connect('mongodb://localhost:27017/gymBackend')
-.then(() => console.log('DB connection successful!')).catch(err=>{
-  console.log(err)
-});
+const connectDB = async () => {
+    try {
+        // This line forces the app to use your MONGO_URI from the environment
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+
+        console.log(`DB connection successful! Host: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`DB Connection Error: ${error.message}`);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+module.exports = connectDB;
